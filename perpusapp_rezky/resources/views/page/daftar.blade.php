51ebb5084bfdf7e693050d623f476d994ce44caa
rezkyputra <rezky.putra46@gmail.com>
2026-02-05 11:11:01 +0800

    templating dan migration

--- perpusapp_rezky/resources/views/page/daftar.blade.php
+++ perpusapp_rezky/resources/views/page/daftar.blade.php
@@ -1,23 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Halaman Daftar</h1>
-    <a href="/">kembali</a>
-    <form method="POST" action="/home">
-         @csrf
-        <label >Fullname</label> <br>
-        <input type="text" name="fullname" > <br> <br>
-        <label >Alamat</label> <br>
-        <textarea name="address" cols="30" rows="10"></textarea> <br> <br>
+@extends('layouts.master')
+@section('title')
+    Halaman Daftar
+@endsection
+@section('content')
+    
+<a href="/">kembali</a>
+<form method="POST" action="/home">
+     @csrf
+    <label >Fullname</label> <br>
+    <input type="text" name="fullname" > <br> <br>
+    <label >Alamat</label> <br>
+    <textarea name="address" cols="30" rows="10"></textarea> <br> <br>
 
-        <input type="submit" value="kirim">
+    <input type="submit" value="kirim">
 
-    </form>
-</body>
-</html>
+</form>
+@endsection
+    
